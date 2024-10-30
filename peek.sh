f1b76e9299682d76f345248fc32283772769b26c
alexiosvg <alexiosvg@gmail.com>
2024-10-30 23:15:46 +0100

    31/10

--- peek.sh
+++ peek.sh
@@ -2,11 +2,26 @@
 
 # Assign arguments to variables
 FILE="$1"
-LINES="$2"
 
-# Print the first 'LINES' lines of the file specified as the first argument
-head -n "$LINES" "$FILE"
-echo "..."
-# Print the last 'LINES' lines of the file specified as the first argument
-tail -n "$LINES" "$FILE"
+# Check if the second argument is provided, if not, default to 3
+if [ -z "$2" ]; then
+    LINES=3
+else
+    LINES="$2"
+fi
+
+# Get the total number of lines in the file
+TOTAL_LINES=$(cat "$FILE" | wc -l)
+
+# Check if the file has 2X lines or less
+if (( TOTAL_LINES <= 2 * LINES )); then
+    # Print the full content of the file
+    cat "$FILE"
+else
+    # Print a warning message followed by the first and last 'LINES' lines
+    echo "Warning: The file has more than $((2 * LINES)) lines."
+    head -n "$LINES" "$FILE"
+    echo "..."
+    tail -n "$LINES" "$FILE"
+fi
 
